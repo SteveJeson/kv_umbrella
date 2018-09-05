4af517d4ae623e6bfd7c24c034b93b4a753168a6
liuwei <1808289833@qq.com>
2018-09-05 11:49:40 +0800

    add anagrams.exs

--- apps/concurrent/lib/tasks/anagrams.exs
+++ apps/concurrent/lib/tasks/anagrams.exs
@@ -24,3 +24,17 @@ defmodule Dictionary do
     do: word |> to_char_list |> Enum.sort |> to_string
 
 end
+
+defmodule WordListLoader do
+    def load_from_files(file_names) do
+        file_names
+        |>  Stream.map(fn name -> Task.async(fn -> load_task(name) end) end)
+        |> Enum.map(&Task.await/1)
+    end
+
+    defp load_task(file_name) do
+        File.stream!(file_name, [], :line)
+        |> Enum.map(&String.strip/1)
+        |> Dictionary.add_words
+    end
+end
